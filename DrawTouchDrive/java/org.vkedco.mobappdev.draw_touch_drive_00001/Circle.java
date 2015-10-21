63b8395b79bf4dd1fb5ef7250b856bfc15c3caf7
vkedco <vladimir.kulyukin@gmail.com>
2015-10-20 18:13:40 -0600

    Create Circle.java

---
+++ DrawTouchDrive/java/org.vkedco.mobappdev.draw_touch_drive_00001/Circle.java
@@ -0,0 +1,24 @@
+package org.vkedco.mobappdev.draw_touch_drive_00001;
+
+/*************************************************
+ * Circle class defines Circle objects drawn
+ * by TicTacToeView defined in TicTacToeView.java.  
+ * 
+ * The user can drag and drop circles all
+ * over canvas (e.g., place them on the board).
+ * @author: Vladimir Kulyukin
+ *************************************************
+ */
+
+public class Circle extends Shape {
+	final float mRadius;
+	
+	public Circle(float x, float y, float r) {
+		super(x, y);
+		mRadius = r;
+	}
+	
+	final float getRadius() {
+		return mRadius;
+	}
+}
